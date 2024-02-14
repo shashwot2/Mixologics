86cd1ab161db70ed8d87951751ba0b6a27b82aa2
Shashwot KC <shashwot_07@hotmail.com>
2024-02-14 16:03:37 -0500

    Add .env variable config

--- src/components/auth/auth.ts
+++ src/components/auth/auth.ts
@@ -1,20 +1,24 @@
 import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import Config from 'react-native-config';
-GoogleSignin.configure({
-  webClientId: Config.WEB_CLIENT_ID,
-});
-
 export const onAuthStateChanged = (
   callback: (user: FirebaseAuthTypes.User | null) => void,
 ): (() => void) => {
   return auth().onAuthStateChanged(callback);
 };
-
+GoogleSignin.configure({
+	webClientId: Config.WEB_CLIENT_ID,
+});
+ 
 export const signInWithGoogle =
   async (): Promise<FirebaseAuthTypes.UserCredential> => {
+    try {
     await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
     const {idToken} = await GoogleSignin.signIn();
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     return auth().signInWithCredential(googleCredential);
-  };
+    } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
