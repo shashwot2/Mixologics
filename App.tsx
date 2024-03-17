560502f22e841f065a532134214127fc94cf5e23
Shashwot KC <shashwot_07@hotmail.com>
2024-03-17 13:01:26 -0400

    Update mybar with no header

--- App.tsx
+++ App.tsx
@@ -19,9 +19,9 @@ const App: React.FC = () => {
     <NavigationContainer>
       <Stack.Navigator>
         {user ? (
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}} />
         ) : (
-          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{headerShown:false}}/>
         )}
       </Stack.Navigator>
     </NavigationContainer>
